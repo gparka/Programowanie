e07849c4e30a1cd0e804ac54e941e8a56b337b21
Ceglan <74541137+Ceglan@users.noreply.github.com>
2020-12-02 10:10:32 +0100

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -6,6 +6,9 @@ int main(){
     random_device r;
     mt19937 gen(r());
     uniform_int_distribution<int> uniform_dist(1, 6);
-    cout<<uniform_dist(gen);
+    for (int n=0; n<4; ++n)
+    {
+    cout<<uniform_dist(gen)<<' ';
+    }
     return 0;
 }
